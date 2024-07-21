9a1aaacd8dffd4203e83a28e23f55374a83f1207
MichelKosvosi <sharksoareskosvoski@gmail.com>
2024-07-21 03:26:51 -0400

    Projeto web

---
+++ src/main/java/com/petrofil/courser/CourserApplication.java
@@ -0,0 +1,13 @@
+package com.petrofil.courser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourserApplication.class, args);
+	}
+
+}
